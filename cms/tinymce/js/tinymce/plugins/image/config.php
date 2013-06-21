468ab368ad5d1bc2a52582ce6b1421d12d53ba9c
Fons Hettema <ffh_93@live.nl>
2013-06-21 19:16:24 +0200

    TinyMCE

--- cms/tinymce/js/tinymce/plugins/image/config.php
+++ cms/tinymce/js/tinymce/plugins/image/config.php
@@ -3,7 +3,7 @@
 session_start();
 
 /** Full path to the folder that images will be used as library and upload. Include trailing slash */
-define('LIBRARY_FOLDER_PATH', 'uploads/images/');
+define('LIBRARY_FOLDER_PATH', '../../../../../../uploads/images/');
 
 /** Full URL to the folder that images will be used as library and upload. Include trailing slash and protocol (i.e. http://) */
 define('LIBRARY_FOLDER_URL', 'http://www.ios-league.com/uploads/images/');
@@ -13,7 +13,6 @@ define('ALLOWED_IMG_EXTENSIONS', 'gif,jpg,jpeg,png,jpe');
 
 /**  Use these 3 functions to check cookies and sessions for permission. 
 Simply write your code and return true or false */
-
 
 function CanAcessLibrary(){
 	return true;
